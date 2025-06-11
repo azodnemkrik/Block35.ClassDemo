7fb86efe07fffe3988e9deac1eedb88c60acb4c9
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-11 18:55:07 -0500

    Added init()

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const init = () => {
+    const PORT = 3000
+    app.listen(PORT, () => {
+        console.log(`Listening to PORT: ${PORT}`)
+    })
+
+}
+
+init()
